6b79e6d165319809237482a1f43a06a41630e9e5
TheKillerDonut <thekillerdonut1@aol.com>
2014-07-26 18:59:19 +0000

    Renamed "forward" to "next", wrote next and back methods.

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,9 @@
-/* James Moretti
- * July 26, 2014
+/**
+ * Responsible for playing tracks from a PlayList.
  *
- * Player.java - Responsible for playing tracks from a PlayList.
+ * @author James Moretti
+ * @since July 26, 2014
  */
-
 public class Player {
 	private int curTrack;
 	private PlayList curList;
@@ -11,10 +11,38 @@ public class Player {
 
 	public void play(int track) {
 	}
+
+	public void pause() {
+	}
+
+	/**
+	 * Makes a system call to stop playing the current track.
+	 */
 	public void stop() {
+		
 	}
+
+	/**
+	 * Retreats to last track. Loops back to last track at end of play list.
+	 */
 	public void back() {
+		if (curTrack == 0)
+			curTrack = curList.size();
+		else if (curTrack > 0)
+			curTrack--;
+
+		play(curTrack);
 	}
-	public void forward() {
+
+	/**
+	 * Advances to next track. Loops back to first track at end of play list.
+	 */
+	public void next() {
+		if (curTrack < curList.size())
+			curTrack++;
+		else if (curTrack == curList.size())
+			curTrack = 1;
+
+		play(curTrack);
 	}
 }
