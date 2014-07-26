c2a21215f6745bd7726786daf0e11a4a21284886
TheKillerDonut <thekillerdonut1@aol.com>
2014-07-26 18:28:05 +0000

    PlayList reads in tracks from directory and returns their names

--- src/PlayList.java
+++ src/PlayList.java
@@ -1,12 +1,37 @@
-/* James Moretti
- * July 26, 2014
+/**
+ * Loads in and contains play lists from file.
  *
- * PlayList.java - Loads in and contains play lists from file.
+ * @author	James Moretti
+ * @since	July 26, 2014
  */
+import java.util.ArrayList;
+import java.io.File;
 
 public class PlayList {
-	private List tracks;
+	private String[] tracks;
 
-	public void loadPlayList(int list) {
+	/**
+	 * Populates internal track name list using file names in the given
+	 * play list directory.
+	 *
+	 * @param	listNum	Play list to load in
+	 */
+	public void loadPlayList(int listNum) {
+		File f = new File("./tracks/" + listNum);
+		tracks = f.list();
+	}
+
+	/**
+	 * Gets the track name at the given position in the tracks array.
+	 *
+ 	 * @param	trackNum	Track number
+	 * @return	Track name that corresponds to trackNum.
+	 *			Gets last track if trackNum is out of range.
+	 */
+	public String getTrack(int trackNum) {
+		if (trackNum > tracks.length)
+			return tracks[tracks.length - 1];
+		else
+			return tracks[trackNum - 1];
 	}
 }
