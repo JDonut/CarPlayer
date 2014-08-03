eb200afb9c2a1da7d319fdd96ae0afc496944b42
TheKillerDonut <thekillerdonut1@aol.com>
2014-08-03 01:30:44 +0000

    Abstract the system calls required to play mp3s using mpg123 into its own class

---
+++ src/Mpg123Proxy.java
@@ -0,0 +1,32 @@
+/**
+ * Provides a proxy for the system calls required to mp3s using mpg123.
+ * Obviously using this makes your program dependant on linux.
+ *
+ * @author	James Moretti
+ * @since Aug 2, 2014
+ */
+public class Mpg123Proxy implements Runnable {
+	Process syscall;
+
+	public void run() {
+	}
+
+	/**
+	 * Makes a system call to play the specified mp3 using mpg123.
+	 *
+	 * @param	songPath	Relative path to the mp3 file to play.
+	 */
+	public void play(String songPath) {
+		String s = "mpg123 \"" + songPath + "\"";
+		String[] cmd = {"/bin/bash", "-c", s};
+		try {
+			syscall = Runtime.getRuntime().exec(cmd);
+		}
+		catch (Exception e) {
+			System.out.println("Failed to play <" + songPath + ">");
+		}
+	}
+
+	public void stop() {
+	}
+}
