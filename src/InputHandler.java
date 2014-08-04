313759615abccab9c8823ac815a39f15618f3f3d
TheKillerDonut <thekillerdonut1@aol.com>
2014-08-04 00:35:00 +0000

    Set up to handle real-time key press input

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -1,18 +1,30 @@
-/* James Moretti
- * July 26, 2014
+/**
+ * As the name implies, processes input from the keypad. Since the program
+ * is entirely dependent on keypad input, this class holds the main method
+ * and drives the rest of the program.
  *
- * InputHandler.java - As the name implies, processes input from the keypad.
- * Since the program is entirely dependent on keypad input, this class holds
- * the main method and drives the rest of the program.
+ * @author James Moretti
+ * @since July 26, 2014
  */
+import java.io.Reader;
 
 public class InputHandler {
-	private Player player;
+
+	private PlayManager playManager;
+	private final String[] TO_RAW = {"/bin/bash", "-c", "stty raw </dev/tty"};
+	private final String[] TO_COOKED = {"/bin/bash", "-c", "stty cooked </dev/tty"};
+	private final String QUIT_STRING = "quit";
+	private int bufPos = 0;
+	private char[] quitStringBuffer = new char[QUIT_STRING.length()];
 
 	public static void main(String[] args) {
+		
 	}
 
-	public void processInput() {
+	private void getInput() {
+	}
+
+	private void processInput(char c) {
 	}
 
 	private void generateTrackLists() {
