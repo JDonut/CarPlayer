0aa34d6b9c05c1977a8de155c9dee58009be4374
TheKillerDonut <thekillerdonut1@aol.com>
2014-08-05 04:34:04 +0000

    Wrote some (poor) functionality to handle input from the number pad.
    Very basic and fragile at the moment. Also, the code is really just awful.

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -8,6 +8,11 @@ import java.io.Reader;
 
 public class InputHandler {
 
+	// Enums
+	private enum InputState {
+		WAITING, QUEUEING, PLAYING
+	}
+
 	// Constants
 	private final String[] TO_RAW = {"/bin/bash", "-c", "stty raw </dev/tty"};
 	private final String[] TO_COOKED = {"/bin/bash", "-c", "stty cooked </dev/tty"};
@@ -15,8 +20,10 @@ public class InputHandler {
 
 	private PlayManager playManager;
 	private Speaker speaker;
+	private InputState mode;
 	private int bufpos;
 	private char[] quitStringBuffer = new char[QUIT_STRING.length()];
+	private String playListSelection = "";
 
 	/**
 	 * Constructor for InputHandler. Just initializes some objects.
@@ -25,6 +32,7 @@ public class InputHandler {
 		playManager = new PlayManager();
 		speaker = new Speaker();
 		bufpos = 0;
+		mode = InputState.WAITING;
 	}
 
 	/**
@@ -34,6 +42,7 @@ public class InputHandler {
 	 * loop will only be terminated when the device loses power.
 	 */
 	public void runInputLoop() {
+		speaker.speak("Awaiting input");
 		Reader reader = System.console().reader();
 
 		// A few of these commands require their exceptions to be caught so
@@ -49,13 +58,20 @@ public class InputHandler {
 				checkKillString(curchar);
 				processInput(curchar);
 			}
-
-			// Put the console back into cooked input mode
-			Runtime.getRuntime().exec(TO_COOKED).waitFor();
 		}
 		catch (Exception e) {
 			System.err.println("Main loop done broked");
 			e.printStackTrace();
+		}
+		finally {
+			// Put the console back into cooked input mode
+			try {
+				Runtime.getRuntime().exec(TO_COOKED).waitFor();
+			}
+			catch (Exception e2) {
+				System.err.println("Crashed reseting terminal");
+				e2.printStackTrace();
+			}
 		}
 	}
 
@@ -63,6 +79,8 @@ public class InputHandler {
 	 * Checks to see if each character of the quit string has been typed
 	 * consecutively, in order. Keeps a running count of how many characters
 	 * have been matched. Sets it back to 0 if a character doesn't match.
+	 *
+	 * @param	c	character to check.
 	 */
 	private void checkKillString(char c) {
 		if (c == QUIT_STRING.charAt(bufpos)) {
@@ -72,7 +90,54 @@ public class InputHandler {
 		else bufpos = 0;
 	}
 
+	/**
+	 * Checks the given character against a list of commands.
+	 * Executes matched command.
+	 *
+	 * @param	c	character to process.
+	 */
 	private void processInput(char c) {
+		// This is really bad. I'm sorry.
+		switch (c) {
+			case '+': playManager.next(); break;
+			case '-': playManager.back(); break;
+			case '*': System.out.print("[Shuffle]"); break;
+			case '/': System.out.print("[Linear]"); break;
+			case '\r':
+				if (mode == InputState.QUEUEING) {
+					// Turn individual keystrokes into a number
+					int num = Integer.parseInt(playListSelection);
+					playListSelection = ""; // Reset input number
+					playManager.setPlayList(new PlayList(num));
+					playManager.play();
+					mode = InputState.PLAYING;
+				}
+				break;
+			case '.':
+				if (mode == InputState.PLAYING) {
+					playManager.stop();
+					playManager.setPlayList(null);
+					mode = InputState.WAITING;
+				}
+				break;
+			default:
+				if (charIsDigit(c)) {
+					if (mode == InputState.WAITING)
+						mode = InputState.QUEUEING;
+					// Append numbers to string to make an int later
+					playListSelection += c;
+				}
+				break;
+		}
 	}
 
+	/**
+	 * Checks if a the given character is a digit.
+	 * @param	c	character to test.
+	 * @return	true is character is a digit, false if not.
+	 */
+	private boolean charIsDigit(char c) {
+		return c == '0' || c == '1' || c == '2' || c == '3' || c == '4' ||
+				c == '5' || c == '6' || c == '7' || c == '8' || c == '9';
+	}
 }
