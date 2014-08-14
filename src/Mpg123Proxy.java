708757bcc2ca1065892e0bfda7d007ab70546142
TheKillerDonut <thekillerdonut1@aol.com>
2014-08-14 14:09:43 +0000

    mpg123 process is run in its own thread. Also added isPlaying method.

--- src/Mpg123Proxy.java
+++ src/Mpg123Proxy.java
@@ -6,24 +6,37 @@
  * @since Aug 2, 2014
  */
 public class Mpg123Proxy  {
-	private Process syscall;
+	private volatile Process sysCall;
 
 	/**
 	 * Makes a system call to play the specified mp3 using mpg123.
 	 *
+	 * We make the system call in its own thread so we can wait for the system
+	 * call to terminate while the rest of our program continues to run.
+	 * This allows us to check the status of the process by seeing whether or
+	 * not our reference to it is null.
+	 *
 	 * @param	songPath	Relative path to the mp3 file to play.
 	 */
-	public void play(String songPath) {
+	public void play(final String trackPath) {
 		stop(); // Only play one song at a time
 
-		String s = "mpg123 \"" + songPath + "\"";
-		String[] cmd = {"/bin/bash", "-c", s};
-		try {
-			syscall = Runtime.getRuntime().exec(cmd);
-		}
-		catch (Exception e) {
-			System.out.println("Failed to play <" + songPath + ">");
-		}
+		Thread t = new Thread(new Runnable() {
+			public void run() {
+				String s = "mpg123 \"" + trackPath + "\"";
+				String[] cmd = {"/bin/bash", "-c", s};
+				try {
+					sysCall = Runtime.getRuntime().exec(cmd);
+					sysCall.waitFor();
+				}
+				catch (Exception e) {
+					System.out.println("Failed to play <" + trackPath + ">");
+				}
+
+				sysCall = null;
+			}
+		});
+		t.start();
 	}
 
 	/**
@@ -31,10 +44,21 @@ public class Mpg123Proxy  {
 	 */
 	public void stop() {
 		try {
-			syscall.destroy();
+			if (sysCall != null)
+				sysCall.destroy();
 		}
 		catch (Exception e) {
-			// stops compiler whining
+			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * Checks the status of the mpg123 process to determine whether or not a
+	 * track is playing.
+	 *
+	 * @return	true if track is playing, false if not.
+	 */
+	public boolean isPlaying() {
+		return sysCall != null;
 	}
 }
